6810300ac424e0512573f408a194e318584bb7db
teamdodo <36375190+teamdodo@users.noreply.github.com>
2018-02-11 17:31:25 +0100

    Update debug.py

--- debug.py
+++ debug.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import tkinter.ttk as ttk
+import tkinter.messagebox as messagebox
 from constants import *
 from functions import *
 
@@ -12,10 +13,14 @@ class DebugFrame(tk.Frame):
 		self.oldline = "  "
 		self.tags = TAGS
 		self.max_mem = MAX_LUA_MEMORY*1024
+		self.mem = 0
 		self.init_layout()
 		pass
 
-	def init_layout(self):
+	def init_layout(self):	
+		self.labelStatus = tk.Label(self, text="Debugger status: stopped", foreground="red")
+		self.labelStatus.pack(side=tk.TOP, fill=tk.X)
+		
 		self.scrollbar = tk.Scrollbar(self)
 		self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
@@ -23,6 +28,10 @@ class DebugFrame(tk.Frame):
 		self.progressbar.pack(side=tk.BOTTOM, fill=tk.X)
 		self.progressbar["value"] = 0
 		self.progressbar["maximum"] = self.max_mem
+		
+		
+		self.label = tk.Label(self, text=("LUA", "memory","usage",":", self.mem, "of", self.max_mem, "Bytes"))
+		self.label.pack(side=tk.BOTTOM,in_=self.progressbar)
 
 		self.output = tk.Text(self)
 		self.tag_config()
@@ -57,6 +66,9 @@ class DebugFrame(tk.Frame):
 						print(e)
 						exit(1)
 					self.progressbar["value"] = kb*1024+by
+					
+					self.mem = kb*1024+by
+					self.label.config(text=("LUA", "memory","usage",":", self.mem, "of", self.max_mem, "Bytes"))
 				elif "lua" in tmp and not "debug" in tmp:
 					self.output.insert(tk.END, tmp, "info")
 				elif "warn" in tmp:
@@ -69,13 +81,27 @@ class DebugFrame(tk.Frame):
 			self.after(5, self.readfile)
 		pass
 	def start(self):
+		self.labelStatus.config(text="Debugger status: started", foreground="green")
 		self.log_f = open(self.log_path, "r")
 		self.started = True
 		self.readfile()
 		pass
 	def stop(self):
-		self.log_f.close()
-		self.started = False
+		try: #In case of click on "Stop" when debugger has never been started. Otherwise raises exception "DebugFrame has no object log_f"
+			self.log_f.close()
+			self.labelStatus.config(text="Debugger status: stopped", foreground="red")
+			self.started = False
+		except AttributeError:
+			pass
+		pass
+	def clear(self):
+		if not self.started :
+			self.output.delete(1.0,"end")
+			self.progressbar["value"] = 0
+			self.mem = 0
+			self.label.config(text=("LUA", "memory","usage",":", self.mem, "of", self.max_mem, "Bytes"))
+		else:
+			messagebox.showinfo(title="Warning", icon="warning", message="Debugger is running! You have to stop it before you can clear the text.")
 		pass
 
 	def auto_reload(self):
