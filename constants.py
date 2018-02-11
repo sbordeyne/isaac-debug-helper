cca7141597dcfb67b93e60d05b9116d8d4814254
teamdodo <36375190+teamdodo@users.noreply.github.com>
2018-02-11 17:30:45 +0100

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,9 +1,28 @@
 import configparser
 
 OPTIONSOPEN = False
-config = configparser.ConfigParser()
-config.read("config.cfg")
-TAGS = config["TAGS"]
-MAX_LUA_MEMORY = config["GENERAL"].getint("maxmem")
-AUTO_RELOAD = config["GENERAL"].getboolean("autoreload")
-GEOMETRY = config["GEOMETRY"]["x"] + "x" + config["GEOMETRY"]["y"]
+try: 
+	config = configparser.ConfigParser()
+	config.read("config.cfg")
+	TAGS = config["TAGS"]
+	MAX_LUA_MEMORY = config["GENERAL"].getint("maxmem")
+	AUTO_RELOAD = config["GENERAL"].getboolean("autoreload")
+	GEOMETRY = config["GEOMETRY"]["x"] + "x" + config["GEOMETRY"]["y"]
+except KeyError or IOError: 
+	import tkinter.messagebox as messagebox
+	messagebox.showinfo(title="Error", icon="error", message="Config file not found or damaged. Config has been set to default!")
+	config = configparser.ConfigParser()
+	config["TAGS"] = {'error': '#ff0080','warning': '#00FF00', 'info': '#0000FF', 'luadebug': '#000000'}
+	config["GENERAL"] = {}
+	config["GENERAL"]["maxmem"] = "3096"
+	config["GENERAL"]["autoreload"] = "True"
+	config["GEOMETRY"] = {}
+	config["GEOMETRY"]["x"] = "1018"
+	config["GEOMETRY"]["y"] = "882"
+	with open('config.cfg', 'w') as configfile:
+		config.write(configfile)
+	config.read("config.cfg")
+	TAGS = config["TAGS"]
+	MAX_LUA_MEMORY = config["GENERAL"].getint("maxmem")
+	AUTO_RELOAD = config["GENERAL"].getboolean("autoreload")
+	GEOMETRY = config["GEOMETRY"]["x"] + "x" + config["GEOMETRY"]["y"]	
