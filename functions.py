8655fd428a472542c3ab0d12fa08d0bdd2a3bdee
Dogeek <simon.bordeyne@gmail.com>
2018-02-17 21:08:21 +0100

    added check update function in functions.py

--- functions.py
+++ functions.py
@@ -24,9 +24,29 @@ def is_isaac_running():
 			try:
 				if 'isaac-ng' in p.name():
 					return True
-			except psutil.Error: 
+			except psutil.Error:
 				pass
 		return False
 	except  NameError: #NameError in case of psutil not found
 		return False
 
+def is_update_available(current_version):
+	try:
+		import requests
+		from bs4 import BeautifulSoup
+		import re
+		from distutils.version import StrictVersion
+	except ImportError:
+		import tkinter.messagebox as messagebox
+		messagebox.showerror("Error", message="You need BeautifulSoup4, distutils and requests installed for version checking to work.")
+
+	url = "https://github.com/Dogeek/isaac-debug-helper/releases"
+	page = requests.get(url)
+	soup = BeautifulSoup(page.content, "html.parser")
+	versions = [x for x in soup.find_all("span", class_="css-truncate-target")]
+	github_version = versions[-1] if versions else "0.1"
+
+	regex = re.compile(r"([\d.]+)")
+	assert regex.match(current_version) is not None, "Current Version doesn't match the standardized version format (x.x.x.x)"
+	assert regex.match(github_version) is not None, "Github Version doesn't match the standardized version format (x.x.x.x)"
+	return StrictVersion(github_version)>StrictVersion(current_version)
